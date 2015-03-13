4a14c189080eb7e9ed5dd29774a0ff8a2817f92c
joesiewert <joe.siewert@gmail.com>
2015-03-13 14:20:15 -0600

    Setup scrolling on rants

--- app/models/rant.js
+++ app/models/rant.js
@@ -9,7 +9,9 @@ Rant.reopenClass({
   FIXTURES: [
     {id: 1, title: 'Rant One', body: 'This is the first rant.'},
     {id: 2, title: 'Rant Two', body: 'This is the second rant.'},
-    {id: 3, title: 'Rant Three', body: 'This is the third rant.'}
+    {id: 3, title: 'Rant Three', body: 'This is the third rant.'},
+    {id: 4, title: 'Rant Four', body: 'This is the fourth rant.'},
+    {id: 5, title: 'Rant Five', body: 'This is the fifth rant.'}
   ]
 });
 
