26752953ea66997f8ae8cf5e330255df103bf82d
joesiewert <joe.siewert@gmail.com>
2015-03-16 21:27:37 -0600

    Setup save button on new rant

--- app/controllers/rants/new.js
+++ app/controllers/rants/new.js
@@ -2,11 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
+    saveRant: function() {
+      var title = this.get('rantTitle');
+      var body = this.get('rantBody');
+
+      var rant = this.store.createRecord('rant', {title: title, body: body});
+      rant.save().then(function() {
+        this.set('rantTitle', '');
+        this.set('rantBody', '');
+        this.transitionToRoute('rants');
+      }.bind(this));
+    },
+
     cancelRant: function() {
       this.set('rantTitle', '');
       this.set('rantBody', '');
       this.transitionToRoute('rants');
-    },
-
+    }
   }
 });
