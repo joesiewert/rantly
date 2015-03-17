ab9666146054c21aee78f1caad92d381fab6553a
joesiewert <joe.siewert@gmail.com>
2015-03-17 09:21:24 -0600

    Removed fixture data from rant model

--- app/models/rant.js
+++ app/models/rant.js
@@ -1,18 +1,7 @@
 import DS from 'ember-data';
 
-var Rant = DS.Model.extend({
+export default DS.Model.extend({
   title: DS.attr('string'),
-  body: DS.attr('string')
+  body: DS.attr('string'),
+  created_at: DS.attr('date')
 });
-
-Rant.reopenClass({
-  FIXTURES: [
-    {id: 1, title: 'Rant One', body: 'This is the first rant.'},
-    {id: 2, title: 'Rant Two', body: 'This is the second rant.'},
-    {id: 3, title: 'Rant Three', body: 'This is the third rant.'},
-    {id: 4, title: 'Rant Four', body: 'This is the fourth rant.'},
-    {id: 5, title: 'Rant Five', body: 'This is the fifth rant.'}
-  ]
-});
-
-export default Rant;
