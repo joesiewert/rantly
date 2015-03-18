479e9b9a1b649435c37c09e341bd8137a611539b
joesiewert <joe.siewert@gmail.com>
2015-03-18 11:33:53 -0600

    Edit/save existing rant

--- app/controllers/rant.js
+++ app/controllers/rant.js
@@ -8,6 +8,18 @@ export default Ember.ObjectController.extend({
       this.set('isEditing', true);
     },
 
+    saveRant: function(rant) {
+      var title = this.get('title');
+      var body = this.get('body');
+
+      rant.set('title', title);
+      rant.set('body', body);
+      rant.save().then(function() {
+        this.set('isEditing', false);
+        this.transitionToRoute('rants');
+      }.bind(this));
+    },
+
     cancelRant: function() {
       this.set('isEditing', false);
     },
