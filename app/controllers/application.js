67c548d1a8b74678cc154eb93ab179a9d7858404
joesiewert <joe.siewert@gmail.com>
2015-03-19 16:54:25 -0600

    Undoing last commit, will handle on backend instead

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     searchRants: function() {
-      var query = this.get('query').toLowerCase();
+      var query = this.get('query');
       this.transitionToRoute('rants.search', {queryParams: {query: query}});
     }
   }
