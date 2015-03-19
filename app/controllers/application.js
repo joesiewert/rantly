96b8e45f031e93a466f4b0d8ec4777d561d591c1
joesiewert <joe.siewert@gmail.com>
2015-03-19 16:28:31 -0600

    Set search query to lower case

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     searchRants: function() {
-      var query = this.get('query');
+      var query = this.get('query').toLowerCase();
       this.transitionToRoute('rants.search', {queryParams: {query: query}});
     }
   }
