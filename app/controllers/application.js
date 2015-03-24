112bf5e46eab7bd75d815ad2cf657b1bb00f9880
joesiewert <joe.siewert@gmail.com>
2015-03-24 11:57:44 -0600

    Show an error for failed log in

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,8 +3,20 @@ import LoginControllerMixin from 'simple-auth/mixins/login-controller-mixin';
 
 export default Ember.Controller.extend(LoginControllerMixin, {
   authenticator: 'simple-auth-authenticator:devise',
+  loginError: false,
 
   actions: {
+    authenticate: function() {
+      this._super().then(
+        function() {
+          this.set('loginError', false);
+        }.bind(this),
+        function() {
+          this.set('loginError', true);
+        }.bind(this)
+      );
+    },
+
     searchRants: function() {
       var query = this.get('query');
       this.transitionToRoute('rants.search', {queryParams: {query: query}});
