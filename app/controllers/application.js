288fe42f56f9b898c15323825e31e2c5b442f8f9
joesiewert <joe.siewert@gmail.com>
2015-03-27 15:08:02 -0600

    Added validation to search input

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
 import LoginControllerMixin from 'simple-auth/mixins/login-controller-mixin';
+import EmberValidations from 'ember-validations';
 
-export default Ember.Controller.extend(LoginControllerMixin, {
+export default Ember.Controller.extend(LoginControllerMixin, EmberValidations.Mixin, {
   authenticator: 'simple-auth-authenticator:devise',
   loginError: false,
+
+  validations: {
+    query: {
+      presence: {message: "This can't be blank!"}
+    }
+  },
 
   actions: {
     authenticate: function() {
@@ -18,8 +25,15 @@ export default Ember.Controller.extend(LoginControllerMixin, {
     },
 
     searchRants: function() {
-      var query = this.get('query');
-      this.transitionToRoute('rants.search', {queryParams: {query: query}});
+      var input = Ember.$('.nav-search-input')[0];
+
+      if (this.get('isValid') === true) {
+        input.placeholder = 'Search';
+        var query = this.get('query');
+        this.transitionToRoute('rants.search', {queryParams: {query: query}});
+      } else {
+        input.placeholder = this.get('errors.query')[0];
+      }
     }
   }
 });
