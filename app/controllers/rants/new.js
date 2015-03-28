b39e99d216a4739fcb1af2280dc7b0e70261dd07
joesiewert <joe.siewert@gmail.com>
2015-03-28 11:11:44 -0600

    Added validations to new rant

--- app/controllers/rants/new.js
+++ app/controllers/rants/new.js
@@ -1,17 +1,34 @@
 import Ember from 'ember';
+import EmberValidations from 'ember-validations';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(EmberValidations.Mixin, {
+  errorMessages: [],
+
+  validations: {
+    rantTitle: {
+      presence: {message: 'Rants need a title'}
+    },
+    rantBody: {
+      length: {minimum: 144, messages: {tooShort: 'Rants need to be at least 144 characters'}}
+    }
+  },
+
   actions: {
     saveRant: function() {
-      var title = this.get('rantTitle');
-      var body = this.get('rantBody');
+      if (this.get('isValid') === true) {
+        this.set('errorMessages', []);
+        var title = this.get('rantTitle');
+        var body = this.get('rantBody');
 
-      var rant = this.store.createRecord('rant', {title: title, body: body});
-      rant.save().then(function() {
-        this.set('rantTitle', '');
-        this.set('rantBody', '');
-        this.transitionToRoute('rants');
-      }.bind(this));
+        var rant = this.store.createRecord('rant', {title: title, body: body});
+        rant.save().then(function() {
+          this.set('rantTitle', '');
+          this.set('rantBody', '');
+          this.transitionToRoute('rants');
+        }.bind(this));
+      } else {
+        this.set('errorMessages', [(this.get('errors.rantTitle')[0]), (this.get('errors.rantBody')[0])]);
+      }
     },
 
     cancelRant: function() {
