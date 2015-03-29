d4b663bab35eb1911da070a57cbb990dbd5eab2a
joesiewert <joe.siewert@gmail.com>
2015-03-29 13:55:21 -0600

    Setup rant/user associations

--- app/models/user.js
+++ app/models/user.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   username: DS.attr('string'),
   name: DS.attr('string'),
   email: DS.attr('string'),
-  password: DS.attr('string')
+  password: DS.attr('string'),
+  rants: DS.hasMany('rant')
 });
